ca240bff084c6d00c32ea5e8fd7cffdfab260e6e
Marvin <marvin.rosskothen@outlook.de>
2021-10-24 21:17:23 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/min/simple.min.css">
+</head>
+<body>
+
+<div class="box-lg">
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto commodi impedit itaque nesciunt
+        reprehenderit voluptatem voluptatibus? Consequuntur cumque dignissimos dolore impedit iusto molestiae rerum
+        tenetur
+        velit. Cum eaque esse explicabo odio quasi sed voluptas, voluptatum? Aliquam cum dignissimos ducimus fugit
+        inventore
+        laborum mollitia, odio quas quasi quod ratione reiciendis tenetur unde? Ab cupiditate earum fuga iste, itaque
+        quas!
+        Aut eos facilis fugit itaque magnam minima modi placeat sed tenetur.
+    </p>
+</div>
+
+</body>
+</html>
